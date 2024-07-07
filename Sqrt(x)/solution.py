6f432606171dd47d87593b07572b8e88f4a9fafc
bipin-saha <bipinsaha142@gmail.com>
2024-07-07 20:14:10 +0600

    Accepted - Runtime - 15ms, Memory - 11.4MB

---
+++ Sqrt(x)/solution.py
@@ -0,0 +1,12 @@
+#Problem
+#https://leetcode.com/submissions/detail/1312787065/
+
+#Solution
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(sqrt(x))
+        
